03cea301635c3a79af738f6aba00a4c14839f560
Feyi Akomolafe <113041424+Feyi-Akomolafe@users.noreply.github.com>
2024-04-30 08:56:08 -0400

    changed code

--- Typescript/index.ts
+++ Typescript/index.ts
@@ -1,74 +1,57 @@
 /**
- * The program is the mean and median calculator for unit-1-06
+ * The program is the classic mark generator for 107
  *
  * By: Feyi Akomolafe
  * Version: 1.0
- * Since: 2024-03-18
+ * Since: 2024-2-18
  */
 
-/**
- * @param {Array<number>} listArray parameter one as a number
- * @returns {number} return mean value as a number
- */
-function MeanCalculation(listArray: number[]): number {
-  // process
-  let sum = 0;
-  const arraySize = listArray.length;
-  for (let loopCounterTwo = 0; loopCounterTwo < arraySize; loopCounterTwo++) {
-    sum += listArray[loopCounterTwo];
-  }
-  const mean = sum / arraySize;
-  return mean;
+import { writeFileSync } from "fs"
+
+function generateGaussian(mean, std){
+  // https://discourse.psychopy.org/t/javascript-gaussian-function/17724/2
+  var _2PI = Math.PI * 2;
+  var u1 = Math.random();
+  var u2 = Math.random();
+  
+  var z0 = Math.sqrt(-2.0 * Math.log(u1)) * Math.cos(_2PI * u2);
+  var z1 = Math.sqrt(-2.0 * Math.log(u1)) * Math.sin(_2PI * u2);
+
+  return z0 * std + mean;
 }
 
-/**
- * @param {Array<number>} listArray parameter one as a number
- * @returns {number} return median value as a number
- */
-function MedianCalculation(listArray: number[]): number {
-  // process
-  let median = 0;
+// Add first row
+let array = 'Name,'
+for (let counter = 1; counter < 9; counter ++) {
+  array = array + ('Assignment' + counter) + ','
+}
+array = array + "\n"
 
-  const sortedArray = listArray.sort(function(a, b) {
-    return a - b;
-  });
-
-  const arraySize = listArray.length;
-
-  if (arraySize % 2 === 0) {
-    median = (sortedArray[arraySize / 2] + sortedArray[arraySize / 2 - 1]) / 2;
+// Generate marks
+let sum = 0
+let listCount = 0
+let studentCount = 1
+for (var counter = 0; counter < 24; counter++) {
+  const normalNumber = generateGaussian(75, 10)
+  sum = sum + normalNumber
+  if (listCount == 0) {
+    array = array + ('Student' + studentCount) + ','
+    studentCount++
+  }
+  array = array + Math.floor(normalNumber) + ','
+  if (listCount == 7) {
+    array = array + '\n'
+    listCount = 0
   } else {
-    median = sortedArray[(arraySize - 1) / 2];
+    listCount++
   }
-  return median;
 }
 
-// Check if a file path is provided as a command-line argument
-if (process.argv.length < 3) {
-  console.error('Please provide a file path as a command-line argument.');
-  process.exit(1);
-}
+// Calculate average
+let average = sum / counter
 
-const filePath = process.argv[2];
+console.log("\nMark Average: " + average.toFixed(2) + "\n")
+console.log(array)
+writeFileSync("Marks.csv", array)
 
-// Read the file contents
-const fileContent = readFileSync(filePath, 'utf8');
-
-const newArray = fileContent.split(/\r?\n/);
-// pop last element, since it will be empty (the EOF)
-newArray.pop();
-
-const listArray = [];
-
-for (
-  let loopCounterOne = 0;
-  loopCounterOne < newArray.length;
-  loopCounterOne++
-) {
-  listArray.push(Number(newArray[loopCounterOne]));
-}
-
-console.log(The mean is: ${MeanCalculation(listArray)});
-console.log(The median is: ${MedianCalculation(listArray)});
-
-console.log('\nDone.');
+console.log("\nDone.")
